6bfc28e414a6baeb3c93a74ebc32f272ca8d852d
Manuel Rigger <manuel.rigger@jku.at>
2018-01-26 17:24:33 +0100

    Fix MPX compilation issues

--- mpx.c
+++ mpx.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <limits.h>
 
 ssize_t _size_right(void* p) {
-	return (ssize_t) (__builtin___bnd_get_ptr_ubound(p) + 1) - (ssize_t) p;
+    if ((ssize_t) __builtin___bnd_get_ptr_ubound(p) == -1) {
+        /* No bounds information available */
+        return LONG_MAX;
+    }
+	size_t size = (size_t) (__builtin___bnd_get_ptr_ubound(p) + 1) - (size_t) p;
+    return (ssize_t) size;
 }
