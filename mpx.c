541b7ae7bfaec5f17db4b348c29f4473f6143c44
Daniel Pekarek <pekarekdaniel@gmail.com>
2018-01-31 09:45:15 +0100

    Check if CPU supports MPX

--- mpx.c
+++ mpx.c
@@ -1,11 +1,34 @@
+#include <cpuid.h>
 #include <stdio.h>
 #include <limits.h>
 
-ssize_t _size_right(void* p) {
-    if ((ssize_t) __builtin___bnd_get_ptr_ubound(p) == -1) {
-        /* No bounds information available */
-        return LONG_MAX;
-    }
+static int has_mpx = 0;
+
+void __attribute__((constructor)) __safec_init_mpx(void)
+{
+	unsigned int a, b, c, d;
+	if (!__get_cpuid(0x7, &a, &b, &c, &d)) {
+		fprintf(stderr, "Warning: CPUID request 0x7 unsupported!\n");
+		has_mpx = 0;
+		return;
+	}
+	has_mpx = b & bit_MPX;
+	if(!has_mpx) {
+		fprintf(stderr, "Warning: MPX is unsupported on this CPU\n");
+	}
+}
+
+ssize_t _size_right(void* p)
+{
+	if((ssize_t) __builtin___bnd_get_ptr_ubound(p) == -1) {
+		/* No bounds information available */
+		return LONG_MAX;
+	}
+
+	if(!has_mpx) {
+		return LONG_MAX;
+	}
+
 	size_t size = (size_t) (__builtin___bnd_get_ptr_ubound(p) + 1) - (size_t) p;
-    return (ssize_t) size;
+	return (ssize_t) size;
 }
