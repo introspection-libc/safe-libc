e4878cb38a47b13403d1e4f75ab13eb7f71df7f5
Manuel Rigger <manuel.rigger@jku.at>
2018-02-06 12:01:49 +0100

    Remove the incorrect MPX check

--- mpx.c
+++ mpx.c
@@ -2,33 +2,14 @@
 #include <stdio.h>
 #include <limits.h>
 
-static int has_mpx = 0;
-
-void __attribute__((constructor)) __safec_init_mpx(void)
-{
-	unsigned int a, b, c, d;
-	if (!__get_cpuid(0x7, &a, &b, &c, &d)) {
-		fprintf(stderr, "Warning: CPUID request 0x7 unsupported!\n");
-		has_mpx = 0;
-		return;
-	}
-	has_mpx = b & bit_MPX;
-	if(!has_mpx) {
-		fprintf(stderr, "Warning: MPX is unsupported on this CPU\n");
-	}
-}
-
 ssize_t _size_right(void* p)
 {
-	if((ssize_t) __builtin___bnd_get_ptr_ubound(p) == -1) {
+    ssize_t upper_bounds = (ssize_t) __builtin___bnd_get_ptr_ubound(p);
+	if (upper_bounds == -1) {
 		/* No bounds information available */
 		return LONG_MAX;
 	}
 
-	if(!has_mpx) {
-		return LONG_MAX;
-	}
-
-	size_t size = (size_t) (__builtin___bnd_get_ptr_ubound(p) + 1) - (size_t) p;
-	return (ssize_t) size;
+	size_t size = (size_t) (upper_bounds + 1) - (size_t) p;
+    return (ssize_t) size;
 }
